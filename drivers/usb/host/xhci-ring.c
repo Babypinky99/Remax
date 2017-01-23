1ce8880e36a90c006c5f198f9e8987f154da124f
Felipe Balbi <felipe.balbi@linux.intel.com>
2017-01-23 14:20:20 +0200

    usb: host: xhci: add urb_enqueue/dequeue/giveback tracers
    These three new tracers will help us tie TRBs into URBs by *also*
    looking into URB lifetime.
    
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Change-Id: I59a5137c0ba055131d904b6cbf9cef182162df39

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -663,6 +663,7 @@ static void xhci_giveback_urb_in_irq(struct xhci_hcd *xhci,
 	spin_unlock(&xhci->lock);
 	trace_xhci_urb_giveback(urb);
 	usb_hcd_giveback_urb(hcd, urb, status);
+	trace_xhci_urb_giveback(urb);
 	spin_lock(&xhci->lock);
 }
 
