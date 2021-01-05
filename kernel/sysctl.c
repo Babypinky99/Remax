fed051711ad2bdde0da1dd82c6c7eb82121d67b2
alk3pInjection <webmaster@raspii.tech>
2021-01-06 01:30:25 +0800

    mm: Hard-code maximum swappiness to 200
    Change-Id: I6047d6b267364cc7b291c0dbf87c9ebdc4c58ec9
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -137,6 +137,7 @@ static int two_hundred = 200;
 #endif /* CONFIG_OPLUS_MM_HACKS */
 
 static int __maybe_unused one_thousand = 1000;
+static int max_swappiness = 200;
 #ifdef CONFIG_SCHED_WALT
 static int two_million = 2000000;
 #endif
@@ -1709,7 +1710,7 @@ static struct ctl_table vm_table[] = {
 #ifdef CONFIG_OPLUS_MM_HACKS
 		.extra2         = &two_hundred,
 #else
-		.extra2		= &one_hundred,
+		.extra2		= &max_swappiness,
 #endif /* CONFIG_OPLUS_MM_HACKS */
 	},
 #ifdef CONFIG_OPLUS_MM_HACKS
