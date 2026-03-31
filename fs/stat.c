3fab71c28839996446b2d124cf4bd0414706bd21
Pranav Vashi <neobuddy89@gmail.com>
2026-03-31 11:16:10 +0530

    drivers: Add kernelsu hooks
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/stat.c
+++ fs/stat.c
@@ -111,6 +111,9 @@ int vfs_fstat(unsigned int fd, struct kstat *stat)
 	return error;
 }
 EXPORT_SYMBOL(vfs_fstat);
+#ifdef CONFIG_KSU
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+#endif
 
 int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 		int flag)
@@ -319,6 +322,10 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 
+#ifdef CONFIG_KSU
+	ksu_handle_stat(&dfd, &filename, &flag);
+#endif
+
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
 		return error;
@@ -460,6 +467,10 @@ SYSCALL_DEFINE4(fstatat64, int, dfd, const char __user *, filename,
 {
 	struct kstat stat;
 	int error;
+
+#ifdef CONFIG_KSU
+	ksu_handle_stat(&dfd, &filename, &flag);
+#endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
