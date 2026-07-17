kerneltoast/android_kernel_google_floral

drm/msm: Remove bogus NULL check in _msm_drm_commit_work_cb()
The work pointer will never be NULL. Remove this check.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

drivers/gpu/drm/msm/msm_atomic.c
@@ -574,18 +574,12 @@ static void complete_commit(struct msm_commit *c)
 
 static void _msm_drm_commit_work_cb(struct kthread_work *work)
 {
-	struct msm_commit *commit =  NULL;
+	struct msm_commit *commit = container_of(work, typeof(*ccommit),
+						 commit_work);
 	struct pm_qos_request req = {
 		.type = PM_QOS_REQ_AFFINE_CORES,
 		.cpus_affine = BIT(raw_smp_processor_id())
 	};
-
-	if (!work) {
-		DRM_ERROR("%s: Invalid commit work data!\n", __func__);
-		return;
-	}
-
-	commit = container_of(work, struct msm_commit, commit_work);
 
 	/*
 	 * Optimistically assume the current task won't migrate to another CPU

Babypinky99/Remax

Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

drivers/gpu/drm/msm/msm_atomic.c
@@ -606,18 +606,12 @@ static void complete_commit(struct msm_commit *c)
 
 static void _msm_drm_commit_work_cb(struct kthread_work *work)
 {
-	struct msm_commit *commit =  NULL;
+	struct msm_commit *commit = container_of(work, typeof(*commit),
+						 commit_work);
 	struct pm_qos_request req = {
 		.type = PM_QOS_REQ_AFFINE_CORES,
 		.cpus_affine = ATOMIC_INIT(BIT(raw_smp_processor_id()))
 	};
-
-	if (!work) {
-		DRM_ERROR("%s: Invalid commit work data!\n", __func__);
-		return;
-	}
-
-	commit = container_of(work, struct msm_commit, commit_work);
 
 	/*
 	 * Optimistically assume the current task won't migrate to another CPU