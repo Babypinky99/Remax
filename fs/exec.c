f28fca9d8ca3a596816b538a4c1c071a10eeeaf5
lightyagami <markrayon5@gmail.com>
2026-08-22 15:50:36 +0530

    Silvercore : KsuNext and 75hz

--- fs/exec.c
+++ fs/exec.c
@@ -1687,6 +1687,11 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+#ifdef CONFIG_KSU
+	extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags);
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
+
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
